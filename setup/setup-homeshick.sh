f44712a0f9ebd811c5d92ad6de467f3f7d8d9633
Daniel Goude <daniel@goude.se>
2020-06-11 10:53:52 +0200

    First commit.

---
+++ setup/setup-homeshick.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+REPOS=$HOME/.homesick/repos
+git clone https://github.com/goude/homeshick.git "$REPOS/homeshick"
+source "$REPOS/homeshick/homeshick.sh"
+
+echo "Cloning homeshick repos..."
+
+homeshick_repos=(
+    "goude/adages"
+)
+
+for i in "${homeshick_repos[@]}"
+do
+    homeshick --force --batch clone "$i"
+done
+
+echo "Silently and forcefully linking homeshick..."
+homeshick --force --batch --quiet link
+
+echo "Done."
