445879c523b3d552693bf7380da39bee8812f215
Daniel Goude <daniel@goude.se>
2020-06-18 22:03:17 +0200

    update

--- setup/rcpy/setup-with-pyenv.sh
+++ setup/rcpy/setup-with-pyenv.sh
@@ -3,6 +3,10 @@
 # this allows pyenv to see Homebrew's python version
 ln -s /usr/local/Cellar/python/* ~/.pyenv/versions/
 
+# to detect system python:
+# sudo ln -s /usr/bin/python3 /usr/bin/python
+# or use python3-is-python package
+
 pyenv virtualenv-delete neovim3
 
 pyenv virtualenv system neovim3
