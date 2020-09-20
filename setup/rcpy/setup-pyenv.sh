205d0aa0aecb6daae00d84b4182321bdfc06afa1
Daniel Goude <daniel@goude.se>
2020-09-20 20:34:45 +0200

    update

--- setup/rcpy/setup-pyenv.sh
+++ setup/rcpy/setup-pyenv.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+	xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
