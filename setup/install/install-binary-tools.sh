b14a19d8d5d725a0132673442dae95828d7a3835
Daniel Goude <daniel@goude.se>
2020-11-18 21:21:24 +0100

    update

--- setup/install/install-binary-tools.sh
+++ setup/install/install-binary-tools.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# echo "Installing starship..."
 curl -fsSL https://starship.rs/install.sh | bash -s -- --bin-dir="$HOME/bin"
+
+echo "shellcheck, shfmt for editing (TODO)"
