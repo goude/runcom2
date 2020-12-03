032568a0ac5862baef41e5d0d43d6457a6e2e923
Daniel Goude <daniel@goude.se>
2020-12-03 20:25:41 +0100

    update

--- home/.config/fish/functions/afs_stuff.fish
+++ home/.config/fish/functions/afs_stuff.fish
@@ -1,6 +1,6 @@
 function afs_stuff --on-variable PWD --description 'Do afs stuff'
   status --is-command-substitution; and return
   if test -e .afs_look
-    cat .afs_look
+    bat --language="md" --style="plain" .afs_look
   end
 end
