fa24243ff0048b0f920f343951603bd2363368bf
Daniel Goude <daniel@goude.se>
2021-04-09 15:27:03 +0200

    update

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,3 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    #curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    curl https://raw.githubusercontent.com/jorgebucaran/fisher/main/fisher.fish --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 export HOMESHICK_REPOS=$HOME/.homesick/repos
 source $HOMESHICK_REPOS/runcom2/localenv
 
