9b7ec1164eb8ca84e12e0df572ea0dab660814a4
Daniel Goude <daniel@goude.se>
2021-07-18 10:42:41 +0200

    update

--- setup/install/update-node-packages.sh
+++ setup/install/update-node-packages.sh
@@ -2,7 +2,9 @@
 source "$HOME/.nvm/nvm.sh"
 nvm use node
 
-npm install -g npm@latest jsonlint eslint eslint-plugin-react tern neovim prettier stylelint stylelint-config-recommended yarn textlint remark-cli remark-preset-lint-recommended
+npm install -g npm@latest jsonlint eslint eslint-plugin-react tern neovim prettier stylelint stylelint-config-recommended yarn textlint remark-cli remark-preset-lint-recommended remark-preset-lint-markdown-style-guide
+
+# remark-preset-lint-markdown-style-guide for hyphens instead of asterixii
 
 #npm install -g npm@latest htmlhint jsonlint base16-builder-node eslint eslint-plugin-react tern tldr neovim prettier stylelint
 
