9edd7453d80e4baa02a37d2d0dd86e06c31f98cb
Daniel Goude <daniel@goude.se>
2021-07-25 18:41:54 +0200

    update

--- home/.config/fish/functions/zse.fish
+++ home/.config/fish/functions/zse.fish
@@ -1,3 +1,3 @@
 function zse
-    zktools ls | fzf --header 'read the snelling man fzf to extend this' --ansi --preview 'bat --style=grid -f {-1}'
+    zktools ls | fzf --header 'Enter to edit file' --with-nth='1,3..' --tac --ansi --preview 'bat --style=grid -f {-1}' --bind 'enter:execute(nvim {-1})'
 end
