c081b06ff9229607e53f0646b9dd2b7aa399988c
Daniel Goude <daniel@goude.se>
2021-07-25 13:16:19 +0200

    update

--- home/.config/fish/functions/zse.fish
+++ home/.config/fish/functions/zse.fish
@@ -1,3 +1,3 @@
 function zse
-    zktools list | fzf --header 'read the snelling man fzf to extend this' --ansi --preview 'bat --style=grid -f {-1}/data.md'
+    zktools ls | fzf --header 'read the snelling man fzf to extend this' --ansi --preview 'bat --style=grid -f {-1}'
 end
