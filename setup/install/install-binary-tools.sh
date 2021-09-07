5a7ee6d8efaf2d8e4e4bd4c5e94c76719d35f482
Daniel Goude <daniel@goude.se>
2021-09-07 06:33:57 +0200

    update

--- setup/install/install-binary-tools.sh
+++ setup/install/install-binary-tools.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# echo "Installing starship..."
+echo "Installing starship..."
 curl -fsSL https://starship.rs/install.sh | bash -s -- --bin-dir="$HOME/bin"
 
-echo "shellcheck, shfmt for editing (TODO)"
+echo "Installing shfmt..."
+curl -L https://github.com/mvdan/sh/releases/download/v3.3.1/shfmt_v3.3.1_linux_amd64 -o "$HOME/shfmt"
+chmod a+x "$HOME/shfmt"
