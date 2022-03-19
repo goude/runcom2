b614983fddd6c34c9eb6dfe0f99af7921dc1104a
Daniel Goude <daniel@goude.se>
2022-03-19 06:33:13 +0100

    update

---
+++ home/.config/nvim/install_lang_servers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+npm install -g bash-language-server pyright vscode-langservers-extracted typescript typescript-language-server
---
+++ home/.config/nvim/install_packer.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+git clone --depth 1 https://github.com/wbthomason/packer.nvim /home/mind/.local/share/nvim/site/pack/packer/start/packer.nvim
