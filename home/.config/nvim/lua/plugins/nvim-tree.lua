d231330c139f50a0a7920b868872d3a3e129357d
Daniel Goude <daniel@goude.se>
2022-06-15 15:30:06 +0200

    disable some nvim-tree settings

--- home/.config/nvim/lua/plugins/nvim-tree.lua
+++ home/.config/nvim/lua/plugins/nvim-tree.lua
@@ -14,20 +14,20 @@
 --- See: `help NvimTree`
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-}
+--g.nvim_tree_indent_markers = 1
+--g.nvim_tree_git_hl = 1
+--g.nvim_tree_highlight_opened_files = 1
+--g.nvim_tree_respect_buf_cwd = 1
+--g.nvim_tree_width_allow_resize  = 1
+--g.nvim_tree_show_icons = {
+--  git = 1,
+--  folders = 1,
+--  files = 1,
+--}
 
-g.nvim_tree_icons = {
-	default = "‣ "
-}
+--g.nvim_tree_icons = {
+--	default = "‣ "
+--}
 
 require('nvim-tree').setup {
   open_on_setup = true,
