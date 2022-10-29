7a7797bb36a39095e96da74136767c79f2dcd119
Daniel Goude <daniel@goude.se>
2022-10-29 10:58:16 +0200

    add charm stuff to installer

--- setup/install/install-binary-tools.sh
+++ setup/install/install-binary-tools.sh
@@ -8,6 +8,12 @@ echo "Installing shfmt..."
 curl -L https://github.com/mvdan/sh/releases/download/v3.3.1/shfmt_v3.3.1_linux_amd64 -o "$HOME/bin/shfmt"
 chmod a+x "$HOME/bin/shfmt"
 
-echo "Installing rust-analyzer"
+echo "Installing rust-analyzer..."
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/bin/rust-analyzer
 chmod +x ~/bin/rust-analyzer
+
+echo "Installing charm stuff..."
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow skate gum vhs
