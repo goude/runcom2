ad0097c0c130d50f8fd9d9ae1d7bba3869db1450
Daniel Goude <daniel@goude.se>
2022-11-03 17:29:15 +0100

    update nvim installation script

--- setup/install/install-nvim.sh
+++ setup/install/install-nvim.sh
@@ -4,7 +4,9 @@ set -e
 cd ~/tmp/
 
 wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
-dpkg -i nvim-linux64.deb
+sudo dpkg -i nvim-linux64.deb
+
+rm -rf ~/.local/share/nvim
 
 cd ~/.config/nvim
 ~/.config/nvim/install_packer.sh
