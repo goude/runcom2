0b595a749f7622276957027b12af76458411d52a
Daniel Goude <daniel@goude.se>
2023-02-13 11:03:09 +0100

    updates

--- home/.config/fish/functions/afs_stuff.fish
+++ home/.config/fish/functions/afs_stuff.fish
@@ -2,9 +2,9 @@ function afs_stuff --on-variable PWD --description 'Do afs stuff'
   status --is-command-substitution; and return
   if test -e notes.md
     # bat --language="md" --style="plain" notes.md
-    glow notes.md
+    batcat notes.md
   end
   if test -e todo.txt
-    bat todo.txt
+    batcat todo.txt
   end
 end
