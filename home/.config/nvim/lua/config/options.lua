86495e3e0acec26e8e1614890adb9ae1535a8920
Daniel Goude <daniel@goude.se>
2023-12-01 17:34:00 +0100

    switch to lazyvim

---
+++ home/.config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Fix problem with Esc triggering Alt+j, Alt+k behavior
+-- https://www.johnhawthorn.com/2012/09/vi-escape-delays/
+vim.opt.ttimeoutlen = 0
