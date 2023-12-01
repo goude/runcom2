af334e45eb7baafa720b2543003c831d965b6d9d
Daniel Goude <daniel@goude.se>
2023-12-01 17:34:49 +0100

    clean up old nvim configs

--- home/.config/nvim-disabled/install_lang_servers.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-npm install -g bash-language-server pyright vscode-langservers-extracted typescript typescript-language-server
--- home/.config/nvim-disabled/install_packer.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-git clone --depth 1 https://github.com/wbthomason/packer.nvim /home/mind/.local/share/nvim/site/pack/packer/start/packer.nvim
