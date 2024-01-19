2b16330f2d6fce802d5af7859eb4d117f8e8c65b
Daniel Goude <daniel@goude.se>
2024-01-19 12:04:09 +0100

    disable alt-j/k keymaps due to tmux issue when pressing esc which triggers an Alt

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "<M-j>", "j")
+vim.keymap.set("i", "<M-k>", "k")
+vim.keymap.set("n", "<M-j>", "j")
+vim.keymap.set("n", "<M-k>", "k")
