63698d86d7e81a2863b92f1e3a3d05cb3aa183f8
Daniel Goude <daniel@goude.se>
2024-11-11 08:13:32 +0100

    ud

--- setup/install/nvim-install.sh
+++ setup/install/nvim-install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.nvim/
 rm -rf ~/.nvim/nvim-linux64
 cd ~/.nvim/
 
-wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 tar xvzf nvim-linux64.tar.gz
 
 rm nvim-linux64.tar.gz
