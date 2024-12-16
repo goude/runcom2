5affbe43b2162fbe3e5bc8a776d769be17c88771
Daniel Goude <daniel@goude.se>
2024-12-16 14:03:03 +0100

    lazyvim ud

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "<M-j>", "j")
-vim.keymap.set("i", "<M-k>", "k")
-vim.keymap.set("n", "<M-j>", "j")
-vim.keymap.set("n", "<M-k>", "k")
+
+-- Disabled - I do not remeber why I set them from the start because they were disabling the nifty up/down line moves
+-- vim.keymap.set("i", "<M-j>", "j")
+-- vim.keymap.set("i", "<M-k>", "k")
+-- vim.keymap.set("n", "<M-j>", "j")
+-- vim.keymap.set("n", "<M-k>", "k")
