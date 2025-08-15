bbbf7fef628cd33907f8ea97ed3ccda374ab221a
Daniel Goude <daniel@goude.se>
2025-08-15 17:08:58 +0200

    update va function

--- home/.config/fish/functions/va.fish
+++ home/.config/fish/functions/va.fish
@@ -1,3 +1,10 @@
 function va
-    source *-venv/bin/activate.fish
+    if test -f .venv/bin/activate.fish
+        source .venv/bin/activate.fish
+    else if test -f *-venv/bin/activate.fish
+        source *-venv/bin/activate.fish
+    else
+        echo "No virtual environment found." >&2
+        return 1
+    end
 end
